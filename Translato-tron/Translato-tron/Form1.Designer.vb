49382a7f3b7681485c4f7362ed98a8e203af1c34
AiryShift <religionofaperturelabs@gmail.com>
2015-02-19 12:30:08 +1100

    Added extremely appropriate triangle.

--- Translato-tron/Translato-tron/Form1.Designer.vb
+++ Translato-tron/Translato-tron/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class mainScreen
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,34 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mainScreen))
+        Me.pbDenmark = New System.Windows.Forms.PictureBox()
+        CType(Me.pbDenmark, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbDenmark
+        '
+        Me.pbDenmark.Location = New System.Drawing.Point(163, 64)
+        Me.pbDenmark.Name = "pbDenmark"
+        Me.pbDenmark.Size = New System.Drawing.Size(150, 81)
+        Me.pbDenmark.TabIndex = 0
+        Me.pbDenmark.TabStop = False
+        '
+        'mainScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.triangle
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(478, 494)
+        Me.Controls.Add(Me.pbDenmark)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "mainScreen"
         Me.Text = "Form1"
+        CType(Me.pbDenmark, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents pbDenmark As System.Windows.Forms.PictureBox
 
 End Class
