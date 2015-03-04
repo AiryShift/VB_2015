a19a3208ed012f14f0c820de5edccbb25d741a94
AiryShift <religionofaperturelabs@gmail.com>
2015-03-05 09:36:37 +1100

    Added background sound
    X-Files theme/10

--- Translato-tron/Translato-tron/exit.vb
+++ Translato-tron/Translato-tron/exit.vb
@@ -2,10 +2,14 @@
 
     Private Sub btnExitFalse_Click(sender As Object, e As EventArgs) Handles btnExitFalse.Click
         Me.Hide()
-        mainScreen.Show()
+        My.Computer.Audio.Stop()
     End Sub
 
     Private Sub btnExitTrue_Click(sender As Object, e As EventArgs) Handles btnExitTrue.Click
         End
+    End Sub
+
+    Private Sub formExit_Load(sender As Object, e As EventArgs) Handles MyBase.Activated
+        My.Computer.Audio.Play(My.Resources.x_files, AudioPlayMode.BackgroundLoop)
     End Sub
 End Class
