b779bc0d2ddc52ce303909f33d5c49e5585e3514
AiryShift <religionofaperturelabs@gmail.com>
2015-11-04 22:55:10 +1100

    Add loadedice project

---
+++ loadedice/loadedice/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.dice1 = New System.Windows.Forms.Label()
+        Me.dice2 = New System.Windows.Forms.Label()
+        Me.results = New System.Windows.Forms.TextBox()
+        Me.press = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'dice1
+        '
+        Me.dice1.AutoSize = True
+        Me.dice1.Location = New System.Drawing.Point(12, 67)
+        Me.dice1.Name = "dice1"
+        Me.dice1.Size = New System.Drawing.Size(39, 13)
+        Me.dice1.TabIndex = 0
+        Me.dice1.Text = "Label1"
+        '
+        'dice2
+        '
+        Me.dice2.AutoSize = True
+        Me.dice2.Location = New System.Drawing.Point(12, 162)
+        Me.dice2.Name = "dice2"
+        Me.dice2.Size = New System.Drawing.Size(39, 13)
+        Me.dice2.TabIndex = 1
+        Me.dice2.Text = "Label2"
+        '
+        'results
+        '
+        Me.results.Location = New System.Drawing.Point(387, 67)
+        Me.results.Multiline = True
+        Me.results.Name = "results"
+        Me.results.Size = New System.Drawing.Size(100, 248)
+        Me.results.TabIndex = 2
+        '
+        'press
+        '
+        Me.press.Location = New System.Drawing.Point(0, 0)
+        Me.press.Name = "press"
+        Me.press.Size = New System.Drawing.Size(75, 23)
+        Me.press.TabIndex = 3
+        Me.press.Text = "Roll"
+        Me.press.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(710, 372)
+        Me.Controls.Add(Me.press)
+        Me.Controls.Add(Me.results)
+        Me.Controls.Add(Me.dice2)
+        Me.Controls.Add(Me.dice1)
+        Me.Name = "main"
+        Me.Text = "Rolling Dice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dice1 As System.Windows.Forms.Label
+    Friend WithEvents dice2 As System.Windows.Forms.Label
+    Friend WithEvents results As System.Windows.Forms.TextBox
+    Friend WithEvents press As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ loadedice/loadedice/roll7.vb
@@ -0,0 +1,37 @@
+﻿Public Class main
+    Dim stats = New Integer(11) {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    Dim random As New Random()
+
+    Private Function throwDie() As Integer
+        Dim num1 As Integer = random.Next() Mod 6 + 1
+        If random.Next() Mod 16 + 1 = 1 Then
+            Return 6
+        End If
+        Return num1
+    End Function
+
+    Private Sub throwAll()
+        dice1.Text = throwDie()
+        dice2.Text = throwDie()
+        stats(CInt(dice1.Text) + CInt(dice2.Text) - 2) += 1
+        results.Text = ""
+        For i = 0 To 10
+            results.AppendText(CStr(i + 2) + ": " + CStr(stats(i)) + Environment.NewLine)
+        Next
+    End Sub
+
+    Private Sub roll_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dice1.Text = "0"
+        dice2.Text = "0"
+        Timer1.Enabled = True
+        Timer1.Interval = 20
+    End Sub
+
+    Private Sub press_Click(sender As Object, e As EventArgs) Handles press.Click
+        throwAll()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        throwAll()
+    End Sub
+End Class
